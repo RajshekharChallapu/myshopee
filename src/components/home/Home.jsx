0517f2e116673761556cc94ad288290b40f00fa0
RajshekharChallapu <32146454+RajshekharChallapu@users.noreply.github.com>
2021-02-04 14:40:40 -0700

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,7 +2,7 @@ function Home() {
   return (
     <div className="home mt-2">
       <h3>Welcome to My Shop!!</h3>
-      <h4>Please click on "Order Status" on header bar to view order progress</h4>
+      <h4>Please click on "Order Status" to view order progress</h4>
     </div>
   );
 }
